3b753a9414a68fc636f3decc12e5dbdcb0176d69
Emmanuele <emagnubcp@gmail.com>
2024-04-07 16:53:49 +0100

    32 - Making a Skill Class (9:39)

--- lib/models/character.dart
+++ lib/models/character.dart
@@ -1,28 +1,14 @@
 //  //   ///
 //  Import LIBRARIES
 //  Import FILES
+import 'skill.dart';
 import 'stats.dart';
 import 'vocation.dart';
 //  //   ///
 
-// class Character {
-//   final String name;
-//   final String slogan;
-//   final String id;
-//   bool _isFav = false;
-
-//   Character({
-//     required this.name,
-//     required this.slogan,
-//     required this.id,
-//   });
-
-//   void tiggleIsFav() {
-//     _isFav = !_isFav;
-//   }
-// }
-
 class Character with Stats {
+  // fields
+  final Set<Skill> skills = {};
   final Vocation vocation;
   final String name;
   final String slogan;
@@ -47,5 +33,10 @@ class Character with Stats {
 // Method
   void tiggleIsFav() {
     _isFav = !_isFav;
+  }
+
+  void updateSkill(Skill skill) {
+    skills.clear();
+    skills.add(skill);
   }
 }
